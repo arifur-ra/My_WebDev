6e1d1cb5af5daf38505264badce4a6c9069064ed
arif202030 <ararif_017@yahoo.com>
2020-09-03 12:54:23 +0200

    update

--- gird_responsive/Exercise/app.js
+++ gird_responsive/Exercise/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const livereload = require("livereload");
 const connectLiveReload = require("connect-livereload");
+// create two server
 const server = express();
 const liveReloadServer = livereload.createServer();
-
+// use middale ware 
 server.use(connectLiveReload());
 server.use(express.static("/home/dci/github_Web/gird_responsive/Exercise"));
+
+// see the live change the browser
 liveReloadServer.watch("/home/dci/github_Web/gird_responsive/Exercise ");
 
 const port = 2022;
