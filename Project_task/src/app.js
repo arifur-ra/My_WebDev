ebb02236324fedb8749d6eb27318b67c4359323a
arifur-ra <ararif_017@yahoo.com>
2020-09-09 16:26:55 +0200

    update

--- Project_task/src/app.js
+++ Project_task/src/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const livereload = require("livereload");
 const connectLiveReload = require("connect-livereload");
+//create Server
 const server = express();
 const liveReloadServer = livereload.createServer();
-
+// middleware
 server.use(connectLiveReload());
 server.use(express.static("public"));
 liveReloadServer.watch("public");
 
-const port = 3132;
+const port = 3133;
 
 server.listen(port, function () {
     console.log(`Server is listening to the port ${port}`);
