34dbc4420340d726ee18541a851673365ca08583
arifur-ra <ararif_017@yahoo.com>
2020-09-22 14:35:14 +0200

    update

---
+++ Ex_sass/ImageSlideShow/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+const path = require("path");
+const {
+    dirname
+} = require("path");
+
+//create Server
+const server = express();
+const liveReloadServer = livereload.createServer();
+
+//use the  middleware function
+server.use(connectLiveReload());
+//console.log(path.join(__dirname, "public"));
+server.use(express.static(path.join(__dirname, "public")));
+//watch the change to inside in folder 'public'
+liveReloadServer.watch(path.join(__dirname, "public"));
+
+const port = 3141;
+
+server.listen(port, function () {
+    console.log(`Server is listening to the port ${port}`);
+});
