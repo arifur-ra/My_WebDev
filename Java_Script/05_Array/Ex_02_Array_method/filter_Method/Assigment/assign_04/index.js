eea95e3297a7564cd0aa6108b93aceee03369dcb
arifur-ra <ararif_017@yahoo.com>
2020-10-30 11:45:41 +0100

    new

--- Java_Script/05_Array/Ex_02_Array_method/filter_Method/Assigment/assign_04/index.js
+++ Java_Script/05_Array/Ex_02_Array_method/filter_Method/Assigment/assign_04/index.js
@@ -1,49 +1,48 @@
-let courses = [
-  {
-    name: "ReactJS",
-    type: "Web development FrontEnd",
-    rank: 2,
-  },
-  {
-    name: "Bootstrap",
-    type: "css",
-    rank: 3,
-  },
-  {
-    name: "JSON",
-    type: "datatype",
-    rank: 2,
-  },
-  {
-    name: "AngularJS",
-    type: "Web development FrontEnd",
-    rank: 4,
-  },
-  {
-    name: "git",
-    type: "vcs",
-    rank: 1,
-  },
-  {
-    name: "NodeJS",
-    type: "Web development BackEnd",
-    rank: 1,
-  },
-  {
-    name: "MongoDB",
-    type: "database",
-    rank: 1,
-  },
-  {
-    name: "MySQL",
-    type: "database",
-    rank: 5,
-  },
-  {
-    name: "VueJS",
-    type: "Web development FrontEnd",
-    rank: 0,
-  },
+let courses = [{
+        name: "ReactJS",
+        type: "Web development FrontEnd",
+        rank: 2,
+    },
+    {
+        name: "Bootstrap",
+        type: "css",
+        rank: 3,
+    },
+    {
+        name: "JSON",
+        type: "datatype",
+        rank: 2,
+    },
+    {
+        name: "AngularJS",
+        type: "Web development FrontEnd",
+        rank: 4,
+    },
+    {
+        name: "git",
+        type: "vcs",
+        rank: 1,
+    },
+    {
+        name: "NodeJS",
+        type: "Web development BackEnd",
+        rank: 1,
+    },
+    {
+        name: "MongoDB",
+        type: "database",
+        rank: 1,
+    },
+    {
+        name: "MySQL",
+        type: "database",
+        rank: 5,
+    },
+    {
+        name: "VueJS",
+        type: "Web development FrontEnd",
+        rank: 0,
+    },
 ];
 
 // please write the functions and uase them in the handlers
@@ -72,9 +71,9 @@ let courses = [
 
 //showData : defining the tool
 function showData(data, Display) {
-  Display.innerHTML = ``;
-  for (let i = 0; i < data.length; i++) {
-    Display.innerHTML += `<table>
+    Display.innerHTML = ``;
+    for (let i = 0; i < data.length; i++) {
+        Display.innerHTML += `<table>
               <tr>
                  <td>${i + 1}</td>
                  <td>${data[i].name}</td>
@@ -82,74 +81,74 @@ function showData(data, Display) {
                  <td>${data[i].rank}</td>
                 </tr>
           </table>  `;
-  }
+    }
 }
 
 function showDataHandler() {
-  // using the tool
-  showData(courses, document.getElementById("display"));
+    // using the tool
+    showData(courses, document.getElementById("display"));
 }
 
 //..................sortByName() -- please create this tool
 
 function sortByName(data) {
-  // take isolated copy keep the reference safe
-  //let sortedByName = data.slice();
-  return data.sort(function (a, b) {
-    //make case insensitive compare
-    if (a.name.toLowerCase() < b.name.toLowerCase()) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
+    // take isolated copy keep the reference safe
+    //let sortedByName = data.slice();
+    return data.sort(function (a, b) {
+        //make case insensitive compare
+        if (a.name.toLowerCase() < b.name.toLowerCase()) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
 }
 
 // sortByNameHandler by using sortBYName()
 
 function sortByNameHandler() {
-  let sorted = sortByName(courses);
-  showData(sorted, document.getElementById("display"));
+    let sorted = sortByName(courses);
+    showData(sorted, document.getElementById("display"));
 }
 
 //...................sortByType() -- tools...................
 
 function sortByType(data) {
-  // take isolated copy keep the reference safe
-  //let sortedByType = data.slice();
-  return data.sort(function (a, b) {
-    //make case insensitive compare
-    if (a.type.toLowerCase() < b.type.toLowerCase()) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
+    // take isolated copy keep the reference safe
+    //let sortedByType = data.slice();
+    return data.sort(function (a, b) {
+        //make case insensitive compare
+        if (a.type.toLowerCase() < b.type.toLowerCase()) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
 }
 
 // sortByTypeHandler by using sortBYName()
 
 function sortByTypeHandler() {
-  let sorted = sortByType(courses);
-  showData(sorted, document.getElementById("display"));
+    let sorted = sortByType(courses);
+    showData(sorted, document.getElementById("display"));
 }
 
 //...............................................
 //sortByType() -- tools
 function sortByRank(data) {
-  // take isolated copy keep the reference safe
-  // let sortedByRank = data.slice();
-  return data.sort(function (a, b) {
-    //make case insensitive compare
-    return a.rank - b.rank;
-  });
+    // take isolated copy keep the reference safe
+    // let sortedByRank = data.slice();
+    return data.sort(function (a, b) {
+        //make case insensitive compare
+        return a.rank - b.rank;
+    });
 }
 
 // sortByTypeHandler by using sortBYName()
 
 function sortByRankHandler() {
-  let sorted = sortByRank(courses);
-  showData(sorted, document.getElementById("display"));
+    let sorted = sortByRank(courses);
+    showData(sorted, document.getElementById("display"));
 }
 
 /**
@@ -160,29 +159,29 @@ function sortByRankHandler() {
 
 // a filter function - tool for item.name
 function filterArrayNames(someArray, word) {
-  return someArray.filter(function (item) {
-    if (item.name.toLowerCase() !== word.toLowerCase()) {
-      return item;
-    }
-  });
+    return someArray.filter(function (item) {
+        if (item.name.toLowerCase() !== word.toLowerCase()) {
+            return item;
+        }
+    });
 
-  // newArray[] Created
+    // newArray[] Created
 }
 
 // Handel the click event of filter button
 function dataFilterNames() {
-  let filterWord = document.getElementById("filter").value; // get HTML Element
-  // use a filter tool
-  // change the original array and and make newArray
-  if (filterWord == "") {
-    alert("Fill the Input Fields Please");
-  } else {
-    courses = filterArrayNames(courses, filterWord);
-  }
-  // re-using the sort by  name tool
-  let sortedNameArray = sortByName(courses);
-  // re-using the show data tool
-  showData(sortedNameArray, document.getElementById("display"));
+    let filterWord = document.getElementById("filter").value; // get HTML Element
+    // use a filter tool
+    // change the original array and and make newArray
+    if (filterWord == "") {
+        alert("Fill the Input Fields Please");
+    } else {
+        courses = filterArrayNames(courses, filterWord);
+    }
+    // re-using the sort by  name tool
+    let sortedNameArray = sortByName(courses);
+    // re-using the show data tool
+    showData(sortedNameArray, document.getElementById("display"));
 }
 
 /**
@@ -193,26 +192,26 @@ function dataFilterNames() {
 // A filter function - tool for item.type
 
 function filterArrayType(someArray, word) {
-  return someArray.filter(function (item) {
-    if (item.type.toLowerCase() != word.toLowerCase()) {
-      return item;
-    }
-  });
+    return someArray.filter(function (item) {
+        if (item.type.toLowerCase() != word.toLowerCase()) {
+            return item;
+        }
+    });
 }
 
 function dataFilterTypes() {
-  let filterWord = document.getElementById("filter").value;
-  // change the original array and and make newArray
-  // use a filter tool
-  if (filterWord == "") {
-    alert("Fill the Input Fields Please");
-  } else {
-    courses = filterArrayType(courses, filterWord);
-  }
-  // re-using the sort by type,tool
-  let sortedTypeArray = sortByType(courses);
-  // re-using the show data tool
-  showData(sortedTypeArray, document.getElementById("display"));
+    let filterWord = document.getElementById("filter").value;
+    // change the original array and and make newArray
+    // use a filter tool
+    if (filterWord == "") {
+        alert("Fill the Input Fields Please");
+    } else {
+        courses = filterArrayType(courses, filterWord);
+    }
+    // re-using the sort by type,tool
+    let sortedTypeArray = sortByType(courses);
+    // re-using the show data tool
+    showData(sortedTypeArray, document.getElementById("display"));
 }
 
 /**
@@ -223,24 +222,34 @@ function dataFilterTypes() {
 // A filter function - tool for item.rank
 
 function filterArrayRank(someArray, word) {
-  return someArray.filter(function (item) {
-    if (item.rank != word) {
-      return item;
-    }
-  });
+    return someArray.filter(function (item) {
+
+        // another way to do 
+        // return item.rank !=word
+        if (item.rank != word) {
+            return item;
+        }
+    });
 }
 
 function dataFilterRanks() {
-  let filterWord = document.getElementById("filter").value;
+    let filterWord = document.getElementById("filter").value;
+    // use a filter tool
+    if (filterWord == "") {
+        alert("Fill the Input Fields Please");
+    } else {
+        courses = filterArrayRank(courses, filterWord);
+    }
 
-  // use a filter tool
-  if (filterWord == "") {
-    alert("Fill the Input Fields Please");
-  } else {
-    courses = filterArrayRank(courses, filterWord);
-  }
+    // re-using the sort by type, name rank tool
+    let sortedRankArray = sortByRank(courses);
+    showData(sortedRankArray, document.getElementById("display"));
+}
 
-  // re-using the sort by type, name rank tool
-  let sortedRankArray = sortByRank(courses);
-  showData(sortedRankArray, document.getElementById("display"));
+function dataFilterRefresh() {
+    display.innerHTML = '';
+}
+
+function dataFilterReset() {
+    showData(courses, document.getElementById("display"));
 }
